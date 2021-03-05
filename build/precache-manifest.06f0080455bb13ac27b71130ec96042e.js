4a3a63ae7b20524c49d91904141820bcedad768c
Brady <phantomskygaming@gmail.com>
2021-03-05 00:12:38 -0800

    commit

---
+++ build/precache-manifest.06f0080455bb13ac27b71130ec96042e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9bbb1cef9914db847afd",
+    "url": "/static/js/main.9bbb1cef.chunk.js"
+  },
+  {
+    "revision": "eded362a4d590446f9e3",
+    "url": "/static/js/1.eded362a.chunk.js"
+  },
+  {
+    "revision": "9bbb1cef9914db847afd",
+    "url": "/static/css/main.82aeb564.chunk.css"
+  },
+  {
+    "revision": "eded362a4d590446f9e3",
+    "url": "/static/css/1.a2b87147.chunk.css"
+  },
+  {
+    "revision": "27a7766bfa31f755dfc4e2ae3d13ed8b",
+    "url": "/index.html"
+  }
+];
